ae5f23ef6f739ac6b608e59aa82ea0e600907aea
Arihant <arihant.singh2025@vitstudent.ac.in>
2026-06-17 14:09:14 +0530

    ok

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f94ab8bb25d37e5a"; }
+/******/ 		__webpack_require__.h = function() { return "30988829be939181"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
